c52b8194e1df5ab8407caf627c5405d7a6c08580
wathmal <sasithawathmal@gmail.com>
2015-03-05 23:02:33 +0530

    initial project files

---
+++ app/cache/dev/twig/87/76/2e943cbb86da1a21252e6047fe082fe47bfd25962113ab0c5c005671a5fd.php
@@ -0,0 +1,59 @@
+<?php
+
+/* testBundle:Default:index.html.twig */
+class __TwigTemplate_87762e943cbb86da1a21252e6047fe082fe47bfd25962113ab0c5c005671a5fd extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        try {
+            $this->parent = $this->env->loadTemplate("::base.html.twig");
+        } catch (Twig_Error_Loader $e) {
+            $e->setTemplateFile($this->getTemplateName());
+            $e->setTemplateLine(1);
+
+            throw $e;
+        }
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        // line 4
+        echo "    <p>howdy,, your id is ";
+        echo twig_escape_filter($this->env, (isset($context["id"]) ? $context["id"] : $this->getContext($context, "id")), "html", null, true);
+        echo "!</p>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "testBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  39 => 4,  36 => 3,  11 => 1,);
+    }
+}
